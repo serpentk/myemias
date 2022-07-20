f1333481a3f86f6f3127c430e07ce52c97de53cc
Katerina N. Shananina <eshananina@rambler-co.ru>
2022-07-20 15:14:08 +0300

    Initial

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to emias
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/emias/core_test.clj
@@ -0,0 +1,7 @@
+(ns emias.core-test
+  (:require [clojure.test :refer :all]
+            [emias.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
