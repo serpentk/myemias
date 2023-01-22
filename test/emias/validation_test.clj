f3070be76a39c98ed92e486204adb49587d5a2db
Katerina N. Shananina <eshananina@rambler-co.ru>
2023-01-22 15:56:24 +0300

    More validation tests

--- test/emias/validation_test.clj
+++ test/emias/validation_test.clj
@@ -16,4 +16,13 @@
     (is (= (check-birthdate "1984-02-30") [false "Wrong day of month"]))
     (is (= (check-birthdate "Давным-давно") [false "Wrong birthdate format"])))
   (testing "Create patient"
-    (is (= (validate-patient patient-data) [true nil]))))
+    (is (= (validate-patient patient-data) [true nil]))
+    (is (= (validate-patient (assoc patient-data :birthdate "Ololo"))
+           [false '("Wrong birthdate format")]))
+    (is (= (validate-patient (assoc patient-data :gender "nothing"))
+           [false '("Gender must be 'f' or 'm'")]))
+    (is (= (validate-patient (dissoc patient-data :policy)) [false '()]))
+    (is (= (validate-patient (dissoc patient-data :name)) [false '()]))
+    (is (= (validate-patient (dissoc patient-data :surname)) [false '()])))
+  (testing "Edit patient"
+    (is (= (validate-patient-edit {:name "Alice"}) [true nil]))))
