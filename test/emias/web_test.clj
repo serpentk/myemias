1b47f121e3e5768662a14b1f065e7b514fca3cb3
Katerina N. Shananina <eshananina@rambler-co.ru>
2023-01-25 10:22:21 +0300

    Add test create patient route

--- test/emias/web_test.clj
+++ test/emias/web_test.clj
@@ -10,11 +10,19 @@
   (testing "Main page"
     (is (= 200 (:status (sut/app {:uri "/" :request-method :get}))))))
 
-(deftest test-api
-  (testing "Test api"
+(deftest test-get
+  (testing "Get empty list"
     (let [response (sut/app {:uri "/patients/"
                              :request-method :get})]
       (is (= (:status response) 200))
       (is (= (json/read-json (:body response))
              {:page {:current 1 :has-next false} :result []})))))
+
+(deftest test-create
+  (testing "Add patient"
+    (let [response (sut/app {:uri "/patients/"
+                             :request-method :post
+                             :headers {"Content-Type" "application/json"}
+                             :body (.getBytes (json/write-str patient-data))})]
+      (is (= 201 (:status response))))))
 
